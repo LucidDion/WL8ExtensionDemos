600387c8718900f36bf67eed9d6ce3bbd370591d
LucidDion <lucid.dion@gmail.com>
2022-04-09 09:45:52 -0400

    Add project files.

---
+++ WL8ExtensionDemos/ClientExtension/WL8DemosClientExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab.WPF;
+
+namespace WL8ExtensionDemos
+{
+    //A WL8ClientExtensionBase can install menus in the WL8 main menu, which can open Child Windows
+    public class WL8DemosClientExtension : WL8ClientExtensionBase
+    {
+        //extension name
+        public override string Name => "WL8DemosClientExtension";
+    }
+}
