4a92e7d00f3abfe2f87993e34904938fe6378844
LucidDion <lucid.dion@gmail.com>
2023-01-02 14:11:52 +0200

    Add Springroll's 2 indicators

---
+++ WL8ExtensionDemos/Indicators/PctOffATH.cs
@@ -0,0 +1,62 @@
+﻿using WealthLab.Core;
+using WealthLab.Indicators;
+
+namespace WealthLab.Community
+{
+    //by Springroll
+    //https://www.wealth-lab.com/Discussion/New-indicators-for-the-WL8-community-9060
+    public class PercentOffATH : IndicatorBase
+    {
+        public PercentOffATH() : base()
+        {
+        }
+
+        public PercentOffATH(BarHistory source) : base()
+        {
+            Parameters[0].Value = source;
+            Populate();
+        }
+
+        public static PercentOffATH Series(BarHistory source)
+        {
+            return new PercentOffATH(source);
+        }
+
+        protected override void GenerateParameters()
+        {
+            AddParameter("Source", ParameterType.BarHistory, PriceComponent.Close);
+        }
+
+        public override string Name { get { return "Percent Off All-time High"; } }
+
+        public override string Abbreviation { get { return "PoATH"; } }
+
+        public override string HelpDescription { get { return "Percent Off All-time High tracks the percentage difference between the previous day’s closing price and the All-Time high price of the entire price history. This metric tells you how far the price has deviated from/converged on the all-time high price."; } }
+
+        public override string PaneTag { get { return "Percent Off All-time High"; } }
+
+        public override PlotStyle DefaultPlotStyle { get { return PlotStyle.Line; } }
+
+        public override WLColor DefaultColor { get { return WLColor.CornflowerBlue; } }
+
+        public override void Populate()
+        {
+            BarHistory bars = Parameters[0].AsBarHistory;
+            DateTimes = bars.DateTimes;
+
+            if (bars.Count == 0 || bars.Count < 10)
+                return;
+
+            double highestClose = Double.MinValue;
+            TimeSeries offAllTimeHigh = new TimeSeries(bars.DateTimes);
+
+            for (int bar = 1; bar < bars.Count; bar++)
+            {
+                if (bars.Close[bar] > highestClose)
+                    highestClose = bars.Close[bar];
+                offAllTimeHigh[bar] = -Math.Abs((highestClose - bars.Close[bar]) * 100.0 / highestClose);
+            }
+            Values = offAllTimeHigh.Values;
+        }
+    }
+}
---
+++ WL8ExtensionDemos/Indicators/RsiWilliamsVixFix.cs
@@ -0,0 +1,62 @@
+﻿using WealthLab.Core;
+using WealthLab.Indicators;
+
+namespace WealthLab.Community
+{
+    //by Springroll
+    //https://www.wealth-lab.com/Discussion/New-indicators-for-the-WL8-community-9060
+    public class RsiWilliamsVixFix : IndicatorBase
+    {
+        public RsiWilliamsVixFix() : base()
+        { }
+
+        public RsiWilliamsVixFix(BarHistory source, int emaPeriod, int rsiPeriod) : base()
+        {
+            Parameters[0].Value = source;
+            Parameters[1].Value = emaPeriod;
+            Parameters[2].Value = rsiPeriod;
+            Populate();
+        }
+
+        public static RsiWilliamsVixFix Series(BarHistory source, int emaPeriod, int rsiPeriod)
+        {
+            return new RsiWilliamsVixFix(source, emaPeriod, rsiPeriod);
+        }
+
+        protected override void GenerateParameters()
+        {
+            AddParameter("Source", ParameterType.BarHistory, PriceComponent.Close);
+            AddParameter("EmaPeriod", ParameterType.Int32, 3);
+            AddParameter("RsiPeriod", ParameterType.Int32, 14);
+        }
+
+        public override string Name { get { return "RSI Williams Vix Fix"; } }
+
+        public override string Abbreviation { get { return "RWVF"; } }
+
+        public override string HelpDescription { get { return "The VixRSI was presented in the January 2014 issue of the TASC magazine. Many years ago, Larry Williams developed something he calls the VIX fix.What he developed was a simple calculation that closely emulates the performance of the original VIX using only price data, and which can therefore be applied to any tradable. In a nutshell, we take the highest close over the last 22 trading days, subtract today’s low price, and then divide the result by the highest close over the last 22 trading days. Combining the VixFix with the RSI makes it even more powerful to detect Instrument bottoms and oversold areas. Detecting overbought VIX \"greed areas\" can also be useful if you try to catch a fast and profitable long trade on the VIX itself."; } }
+
+        public override string PaneTag { get { return "RWVF"; } }
+
+        public override PlotStyle DefaultPlotStyle { get { return PlotStyle.Histogram; } }
+
+        public override WLColor DefaultColor { get { return WLColor.Green; } }
+
+        public override void Populate()
+        {
+            BarHistory bars = Parameters[0].AsBarHistory;
+            Int32 emaPeriod = Parameters[1].AsInt;
+            Int32 rsiPeriod = Parameters[2].AsInt;
+            DateTimes = bars.DateTimes;
+
+            if (bars.Count == 0 || bars.Count < rsiPeriod)
+                return;
+
+            TimeSeries wvf = ((((Highest.Series(bars.Close, 22) >> 1) - bars.Low) / (Highest.Series(bars.Close, 22) >> 1) * 100) + 50);
+            var ema = EMA.Series(wvf, emaPeriod);
+            var rsi = RSI.Series(EMA.Series(bars.Close, emaPeriod), rsiPeriod);
+            var result = ema / rsi;
+            Values = result.Values;
+        }
+    }
+}
